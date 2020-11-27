bbbac63f8c481b89cd6b945d4f63fc3585f268a2
yanfuhai <yan_fuhai@foxmail.com>
2020-11-27 20:17:27 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ mkdir ~/houyi/bin
 mkdir ~/houyi/build
 
 # build collector
-cd "${GOPATH}"/src/houyi/cmd/collector || exit
+cd ./cmd/collector || exit
 go build -v main.go
 mv main ~/houyi/bin/collector
 
 # build agent
-cd "${GOPATH}"/src/houyi/cmd/agent || exit
+cd ../agent || exit
 go build -v main.go
 mv main ~/houyi/bin/agent
