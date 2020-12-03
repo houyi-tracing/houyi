b87d31978d7dbcfe0bd09026258b2038d128ec1c
yan-fuhai <yan_fuhai@hotmail.com>
2020-12-03 10:21:37 +0800

    Update build.sh

--- script/build-all.sh
+++ script/build-all.sh
@@ -7,10 +7,10 @@ mkdir -p ~/houyi/bin/
 
 # build collector
 cd ../cmd/collector || exit
-CGO_ENABLED=0 GOOS=linux GOOsgo build -tags netgo -v main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -tags netgo -v main.go
 mv main ~/houyi/bin/collector
 
 # build agent
 cd ../agent || exit
-CGO_ENABLED=0 GOOS=linux GOOsgo build -tags netgo -v main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -tags netgo -v main.go
 mv main ~/houyi/bin/agent
