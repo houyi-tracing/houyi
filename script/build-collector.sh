78696658a0b063bb74352e57e2ad58d057b80bc2
yan-fuhai <yan_fuhai@hotmail.com>
2020-12-03 10:39:18 +0800

    Update build scripts

--- script/build-collector.sh
+++ script/build-collector.sh
@@ -8,6 +8,5 @@ BUILD_OUT_DIR=~/houyi/${COMPONENT}/
 WORK_DIR=../cmd/${COMPONENT}/
 
 mkdir -p ${BUILD_OUT_DIR}
-CGO_ENABLED=0 GOOS=${OS} GOARCH=${ARCH} go build -v ${WORK_DIR}/main.go
-mv ${WORK_DIR}/main ${BUILD_OUT_DIR}
+CGO_ENABLED=0 GOOS=${OS} GOARCH=${ARCH} go build -o ${BUILD_OUT_DIR}/${COMPONENT} -v ${WORK_DIR}/main.go
 mv ${WORK_DIR}/Dockerfile ${BUILD_OUT_DIR}
