1fc4c418d1888e2c1bfd373769ceb66ab3197940
yan-fuhai <yan_fuhai@hotmail.com>
2020-12-03 10:42:16 +0800

    Update build scripts

--- script/build-agent.sh
+++ script/build-agent.sh
@@ -4,9 +4,9 @@ OS=$1
 ARCH=$2
 
 COMPONENT=agent
-BUILD_OUT_DIR=~/houyi/${COMPONENT}/
-WORK_DIR=../cmd/${COMPONENT}/
+BUILD_OUT_DIR=~/houyi/${COMPONENT}
+WORK_DIR=../cmd/${COMPONENT}
 
 mkdir -p ${BUILD_OUT_DIR}
 CGO_ENABLED=0 GOOS=${OS} GOARCH=${ARCH} go build -o ${BUILD_OUT_DIR}/${COMPONENT} -v ${WORK_DIR}/main.go
-mv ${WORK_DIR}/Dockerfile ${BUILD_OUT_DIR}
+cp ${WORK_DIR}/Dockerfile ${BUILD_OUT_DIR}/
