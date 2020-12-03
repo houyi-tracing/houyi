e7d1138a13ab0af4b18322fb961fde8825bc73d1
yan-fuhai <yan_fuhai@hotmail.com>
2020-12-03 16:15:41 +0800

    Update build-all.sh

--- script/build-all.sh
+++ script/build-all.sh
@@ -3,5 +3,16 @@
 OS=$1
 ARCH=$2
 
+if [[ -z ${OS} ]]; then
+  OS=linux
+fi
+
+if [[ -z ${ARCH} ]]; then
+  ARCH=amd64
+fi
+
+echo "OS=${OS}"
+echo "ARCH=${ARCH}"
+
 ./build-agent.sh ${OS} ${ARCH}
 ./build-collector.sh ${OS} ${ARCH}
