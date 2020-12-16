d54ee677ac846c5a4228d4cdafc4a3d76b21fab5
yan-fuhai <yan_fuhai@hotmail.com>
2020-12-16 12:43:50 +0800

    Fix bugs and update tests

--- script/run-agent.sh
+++ script/run-agent.sh
@@ -2,12 +2,7 @@
 
 ROOT_CMD="./agent"
 
-if [[ $COLLECTOR_SERVICE_HOST ]]; then
-  ROOT_CMD="${ROOT_CMD} --reporter.grpc.host-port=http://houyi-collector:14250 --collector.host=houyi-collector"
-else
-  echo "\$COLLECTOR_SERVICE_HOST must be set"
-  exit -1
-fi
+ROOT_CMD="${ROOT_CMD} --reporter.grpc.host-port=http://houyi-collector:14250 --collector.host=houyi-collector"
 
 echo $ROOT_CMD
 eval $ROOT_CMD
