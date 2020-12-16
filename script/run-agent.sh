1de0d3882cc8bd557343985abb13819283f2c698
yan-fuhai <yan_fuhai@hotmail.com>
2020-12-16 13:13:27 +0800

    Fix bugs and update tests

--- script/run-agent.sh
+++ script/run-agent.sh
@@ -2,7 +2,7 @@
 
 ROOT_CMD="./agent"
 
-ROOT_CMD="${ROOT_CMD} --reporter.grpc.host-port=http://houyi-collector:14250 --collector.host=houyi-collector"
+ROOT_CMD="${ROOT_CMD} --reporter.grpc.host-port=houyi-collector:14250 --collector.host=houyi-collector"
 
 echo $ROOT_CMD
 eval $ROOT_CMD
