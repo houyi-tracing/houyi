29420e19748576d1330f58952c56eb49abb0f2c1
yan-fuhai <yan_fuhai@hotmail.com>
2020-12-16 15:13:32 +0800

    Fix bugs and update tests

--- script/run-agent.sh
+++ script/run-agent.sh
@@ -3,7 +3,7 @@
 ROOT_CMD="./agent"
 
 if [[ $COLLECTOR_SERVICE_HOST ]]; then
-  ROOT_CMD="${ROOT_CMD} --reporter.grpc.host-port=${COLLECTOR_SERVICE_HOST}:${COLLECTOR_SERVICE_PORT_GRPC} --collector.host=${HOUYI_COLLECTOR_SERVICE_HOST}"
+  ROOT_CMD="${ROOT_CMD} --reporter.grpc.host-port=${COLLECTOR_SERVICE_HOST}:${COLLECTOR_SERVICE_PORT_GRPC} --collector.host=${COLLECTOR_SERVICE_HOST}"
 else
   ROOT_CMD="${ROOT_CMD} --reporter.grpc.host-port=houyi-collector:14250 --collector.host=houyi-collector"
 fi
