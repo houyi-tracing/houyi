2c0137d1137dee2d1e7388c555febe99967d6eeb
yan-fuhai <yan_fuhai@hotmail.com>
2021-03-24 14:27:03 +0800

    Fix bugs

--- build/agent/run.sh
+++ build/agent/run.sh
@@ -2,21 +2,5 @@
 
 ROOT_CMD="./agent"
 
-if [[ ${COLLECTOR_ADDR} ]]; then
-  ROOT_CMD="${ROOT_CMD} --collector.addr=${COLLECTOR_ADDR}"
-fi
-if [[ ${COLLECTOR_PORT} ]]; then
-  ROOT_CMD="${ROOT_CMD} --collector.port=${COLLECTOR_PORT}"
-fi
-if [[ ${STRATEGY_MANAGER_ADDR} ]]; then
-  ROOT_CMD="${ROOT_CMD} --strategy.manager.addr=${STRATEGY_MANAGER_ADDR}"
-fi
-if [[ ${STRATEGY_MANAGER_PORT} ]]; then
-  ROOT_CMD="${ROOT_CMD} --strategy.manager.port=${STRATEGY_MANAGER_PORT}"
-fi
-if [[ ${GRPC_LISTEN_PORT} ]]; then
-  ROOT_CMD="${ROOT_CMD} --grpc.listen.port=${GRPC_LISTEN_PORT}"
-fi
-
 echo $ROOT_CMD
 eval $ROOT_CMD
